bac99d60a2c6bd738c49216ed9f19ca2dea9c144
MiguelAlbarracin <miguel.albarracin@magretailgroup.com>
2018-09-13 21:18:05 -0300

    asdasd

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(5000, () => console.log('Example app listening on port 5000!'))
 app.use(express.static('website'));
+
+app
+  .get('/', (req, res) => res.render('website/views/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
