df53d8baf1d16487ddd0674d1fb9db9902be5213
MiguelAlbarracin <miguel.albarracin@magretailgroup.com>
2018-09-14 22:12:33 -0300

    header footer and background

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var bodyParser = require('body-parser');
+var path = require('path');
+const app = express();
 const PORT = process.env.PORT || 5000
 
+/*
+var logger = function(req, res, next){
+  console.log('Logging...');
+}
+app.use(logger);
+*/
 
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/website/views'))
+
+
+//Body parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Static Path
 app.use(express.static('website'));
 
-app
-  .get('/', (req, res) => res.render('website/views/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', function (req, res) {
+  res.render('index',{ title: 'CobraPeak'})
+})
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
